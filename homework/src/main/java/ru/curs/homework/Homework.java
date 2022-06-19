0fcc74400f05338083ec192225b33bd543127905
GeorgyBocharov <georgybocharovfrtc@gmail.com>
2022-06-19 15:52:09 +0300

    Removed private constructor from Homework to run application locally and observe result in kafka conductor app

--- homework/src/main/java/ru/curs/homework/Homework.java
+++ homework/src/main/java/ru/curs/homework/Homework.java
@@ -7,8 +7,6 @@ import org.springframework.kafka.annotation.EnableKafkaStreams;
 @SpringBootApplication
 @EnableKafkaStreams
 public class Homework {
-    private Homework() {
-    }
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Homework.class).headless(false).run(args);
